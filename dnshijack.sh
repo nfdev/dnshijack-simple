d36f4eb8e1bae5f9bb594d0d7bd9d2e3bddb3104
nf.devel <nf.devel@gmail.com>
2014-11-10 23:43:52 +0900

    add script

---
+++ dnshijack.sh
@@ -0,0 +1,24 @@
+#/bin/bash
+
+if [ $# -ne 1 ]; then
+	echo "Usage: <command> <target_domain>"
+	exit 1
+fi
+
+if ! [[ $1 =~  ^[a-zA-Z0-9_.\-]+$ ]];then
+	echo "Check domain name"
+	exit 1
+fi
+
+cd ~/sandbox/dnshijack
+domain=$1
+
+fname="./data/"$domain"_"$( date "+%Y%m%d%H")
+fname_p="./data/"$domain"_"$( date -d "1 hours ago" "+%Y%m%d%H")
+
+nslookup -type=NS $domain > $fname
+
+date "+%Y%m%d%H" >> ./diff.log 2>&1
+diff $fname $fname_p >> ./diff.log 2>&1
+
+
